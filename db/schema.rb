0777a71509ce484c679e8ec29e34e8f1dcb3d056
David Tan <davified@gmail.com>
2016-06-29 09:28:59 +0800

    added bootstrap styling for registration and task forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628123401) do
+ActiveRecord::Schema.define(version: 20160628142032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -49,4 +59,6 @@ ActiveRecord::Schema.define(version: 20160628123401) do
     t.integer  "task_id"
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
 end
