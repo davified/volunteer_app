0008a227c6ce2706fc9ab73c15a882ca8725ca78
David Tan <davified@gmail.com>
2016-07-01 08:41:41 +0800

    fixed volunteer, give and like buttons on task#show page and index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :upvote]
   before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /tasks
